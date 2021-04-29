e06e387f6fe05691cd344473704faa7656bdea69
ThePlumYT <60061341+ThePlumYT@users.noreply.github.com>
2021-04-29 17:47:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const client = new Discord.Client();
 const config = require("./config.json");
 
 
-client.login(config.BOT_TOKEN).catch(console.error);
 client.on('ready', () => {
   console.log('Now Online, ready to filter!');
 });
@@ -15,4 +14,8 @@ client.on('ready', () => {
 client.on('message', message => {
   if(config.FILTER_LIST.some(word => message.content.toLowerCase().includes(word))){
     message.delete()
-  }})
+  }});
+  
+client.login(config.TOKEN);  
+  
+
